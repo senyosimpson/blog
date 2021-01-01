f02e72d8675dd08764175f143294370e266c337e
Senyo <simpsonsenyo@gmail.com>
2021-01-01 17:59:40 +0200

    removed docker folder

--- infra/docker/build_deploy.sh
+++
@@ -1,3 +0,0 @@
-sudo docker build --no-cache -f infra/docker/Dockerfile -t senyo/blog:latest .
-echo $DOCKER_ACCESS_TOKEN | sudo docker login -u $DOCKER_USERNAME --password-stdin
-sudo docker push senyo/blog:latest
